3e79e51e343fae7236bf64bc30c6ae0404237db4
Roman Pushkin <roman.pushkin@gmail.com>
2014-09-11 22:14:14 +0400

    Add stub for /new

--- app.rb
+++ app.rb
@@ -5,3 +5,7 @@ require 'sinatra/reloader'
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
+
+get '/new' do
+	erb "Hello World"
+end
