a4d64e12aba1dc3894add2d9fae81b93c0eee6b6
Roman Pushkin <roman.pushkin@gmail.com>
2014-09-11 23:45:44 +0400

    Select comments to @comments variable

--- app.rb
+++ app.rb
@@ -91,6 +91,9 @@ get '/details/:post_id' do
 	# выбираем этот один пост в переменную @row
 	@row = results[0]
 
+	# выбираем комментарии для нашего поста
+	@comments = @db.execute 'select * from Comments where post_id = ? order by id', [post_id]
+
 	# возвращаем представление details.erb
 	erb :details
 end
